53e8f79dcc221a543b644445148ec40cf0a03308
Mohammed Fonseca <mohammed.fonseca@ti8m.ch>
2020-11-02 00:38:07 +0100

    add barcode scanner support

--- app/src/main/java/ch/mofobo/foodscanner/App.kt
+++ app/src/main/java/ch/mofobo/foodscanner/App.kt
@@ -1,13 +1,19 @@
 package ch.mofobo.foodscanner
 
 import android.app.Application
+import androidx.camera.camera2.Camera2Config
+import androidx.camera.core.CameraXConfig
 import ch.mofobo.foodscanner.di.module.appModule
 import ch.mofobo.foodscanner.di.module.repoModule
 import ch.mofobo.foodscanner.di.module.viewModelModule
 import org.koin.android.ext.koin.androidContext
 import org.koin.core.context.startKoin
 
-class App : Application() {
+class App : Application(), CameraXConfig.Provider {
+
+    override fun getCameraXConfig(): CameraXConfig {
+        return Camera2Config.defaultConfig()
+    }
 
     override fun onCreate() {
         super.onCreate()
--- app/src/main/java/ch/mofobo/foodscanner/features/scanner/camera/CameraScannerFragment.kt
+++ app/src/main/java/ch/mofobo/foodscanner/features/scanner/camera/CameraScannerFragment.kt
@@ -1,18 +1,39 @@
 package ch.mofobo.foodscanner.features.scanner.camera
 
-import android.content.pm.ActivityInfo
+import android.Manifest
+import android.content.pm.PackageManager
 import android.os.Bundle
+import android.util.Log
+import android.util.Size
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.camera.core.CameraSelector
+import androidx.camera.core.ImageAnalysis
+import androidx.camera.core.Preview
+import androidx.camera.lifecycle.ProcessCameraProvider
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.DialogFragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
 import ch.mofobo.foodscanner.R
+import ch.mofobo.foodscanner.features.scanner.camera.analyzer.BarcodeAnalyzer
+import com.google.common.util.concurrent.ListenableFuture
+import kotlinx.android.synthetic.main.fragment_camera_scanner.*
+import java.util.concurrent.Executors
 
 class CameraScannerFragment : DialogFragment() {
 
     private lateinit var cameraScannerViewModel: CameraScannerViewModel
+
+    private lateinit var cameraProviderFuture: ListenableFuture<ProcessCameraProvider>
+    private val cameraExecutor = Executors.newSingleThreadExecutor()
+
+
+    // This is an array of all the permission specified in the manifest.
+    private val REQUIRED_PERMISSIONS = arrayOf(Manifest.permission.CAMERA)
+
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -30,20 +51,96 @@ class CameraScannerFragment : DialogFragment() {
         return root
     }
 
-    override fun onResume() {
-        super.onResume()
-        // requireActivity().requestedOrientation = ActivityInfo.SCREEN_ORIENTATION_SENSOR_LANDSCAPE
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+
+        cameraProviderFuture = ProcessCameraProvider.getInstance(requireContext())
+
+        // Request camera permissions
+        if (allPermissionsGranted()) {
+            cameraProvider()
+        }
 
     }
 
-    override fun onPause() {
-        super.onPause()
-        // requireActivity().requestedOrientation = ActivityInfo.SCREEN_ORIENTATION_UNSPECIFIED
+    override fun onResume() {
+        super.onResume()
+        // requireActivity().requestedOrientation = ActivityInfo.SCREEN_ORIENTATION_SENSOR_LANDSCAPE
+        if (!allPermissionsGranted()) {
+            requireActivity().requestPermissions(REQUIRED_PERMISSIONS, Companion.REQUEST_CODE_PERMISSIONS)
+        }
+
     }
 
 
     override fun getTheme(): Int {
         return R.style.DialogTheme
+    }
+
+    private fun cameraProvider() {
+        cameraProviderFuture.addListener(Runnable {
+            val cameraProvider = cameraProviderFuture.get()
+            startCamera(cameraProvider)
+        }, ContextCompat.getMainExecutor(requireContext()))
+    }
+
+    private fun startCamera(cameraProvider: ProcessCameraProvider) {
+        val cameraSelector = CameraSelector.Builder()
+            .requireLensFacing(CameraSelector.LENS_FACING_BACK)
+            .build()
+
+        val preview = Preview.Builder().apply {
+            setTargetResolution(Size(previewView.width, previewView.height))
+        }.build()
+
+        val imageAnalysis = ImageAnalysis.Builder()
+            .setTargetResolution(Size(previewView.width, previewView.height))
+            .setBackpressureStrategy(ImageAnalysis.STRATEGY_KEEP_ONLY_LATEST)
+            .build()
+            .also {
+                it.setAnalyzer(cameraExecutor, BarcodeAnalyzer { qrResult ->
+                    previewView.post {
+                        Log.d("QRCodeAnalyzer", "Barcode scanned: ${qrResult.text}")
+                        // return wirh ressult
+                    }
+                })
+            }
+
+        cameraProvider.unbindAll()
+
+        val camera = cameraProvider.bindToLifecycle(this, cameraSelector, preview, imageAnalysis)
+        preview.setSurfaceProvider(previewView.createSurfaceProvider(camera.cameraInfo))
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<String>,
+        grantResults: IntArray
+    ) {
+        if (requestCode == Companion.REQUEST_CODE_PERMISSIONS) {
+            if (allPermissionsGranted()) {
+                cameraProvider()
+            } else {
+                Toast.makeText(
+                    requireContext(),
+                    "Permissions not granted by the user.",
+                    Toast.LENGTH_SHORT
+                ).show()
+                // finish()
+            }
+        }
+    }
+
+    private fun allPermissionsGranted() = REQUIRED_PERMISSIONS.all {
+        ContextCompat.checkSelfPermission(requireContext(), it) == PackageManager.PERMISSION_GRANTED
+    }
+
+    companion object {
+        // This is an arbitrary number we are using to keep track of the permission
+    // request. Where an app has multiple context for requesting permission,
+    // this can help differentiate the different contexts.
+        private const val REQUEST_CODE_PERMISSIONS = 10
     }
 
 }
